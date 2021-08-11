a1d4d46000e71acc344fa1c23fe489ce8316ca1b
emonhoque <56671915+emonhoque@users.noreply.github.com>
2021-08-12 01:05:38 +0800

    Added Landing Page

---
+++ sw.js
@@ -0,0 +1,48 @@
+const cacheName = 'landing-cache-v1';
+const staticAssets = [
+  './',
+  './index.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+      caches.open(cacheName)
+          .then(function(cache) {
+              console.log('Opened cache');
+              return cache.addAll(staticAssets);
+          })
+  );
+});
+
+self.addEventListener('activate', e => {
+self.clients.claim();
+});
+
+self.addEventListener('fetch', async e => {
+const req = e.request;
+const url = new URL(req.url);
+
+if (url.origin === location.origin) {
+  e.respondWith(cacheFirst(req));
+} else {
+  e.respondWith(networkAndCache(req));
+}
+});
+
+async function cacheFirst(req) {
+const cache = await caches.open(cacheName);
+const cached = await cache.match(req);
+return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+const cache = await caches.open(cacheName);
+try {
+  const fresh = await fetch(req);
+  await cache.put(req, fresh.clone());
+  return fresh;
+} catch (e) {
+  const cached = await cache.match(req);
+  return cached;
+}
+}
